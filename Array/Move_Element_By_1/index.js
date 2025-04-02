412cb00359dbb89ca19915dd0688a51227e2aaf1
Ronak1215 <ronakchitalia28@gmail.com>
2025-04-02 15:23:28 +0530

    rotation for k times added

--- Array/Move_Element_By_1/index.js
+++ Array/Move_Element_By_1/index.js
@@ -23,3 +23,34 @@ for(let i=arr.length -1 ; i>0; i--){
 
 arr[0] = lastElementCopy;
 console.log(arr);
+
+// this code is for left rotation for k times
+
+let arr = [1,2,3,4,5];
+let kl = 1;
+
+for(let j=0; j<k; j++){
+  let firstElementCopy = arr[0];
+  
+  for(let i=0 ; i<arr.length - 1; i++){
+    arr[i] = arr[i+1];
+  }
+  
+  arr[arr.length - 1] = firstElementCopy;
+}
+console.log(arr);
+
+// this code is for right rotation for k times
+
+let arr = [1,2,3,4,5]
+let kr = 1;
+
+for(let i = 0; i<k ; i++){
+  let lastElementCopy = arr[arr.length - 1];
+  for(let j=arr.length - 1; j>0 ; j--){
+    arr[j] = arr[j - 1]
+  }
+  arr[0] = lastElementCopy
+}
+
+console.log(arr)
