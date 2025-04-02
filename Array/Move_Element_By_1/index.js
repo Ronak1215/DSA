4b03417bb1bc3371e1f98b394f1829e433c5ea8a
Ronak1215 <ronakchitalia28@gmail.com>
2025-04-02 14:21:39 +0530

    right rotation added

--- Array/Move_Element_By_1/index.js
+++ Array/Move_Element_By_1/index.js
@@ -1,10 +1,25 @@
+// this code is for left rotation
+
 let arr = [1,2,3,4,5];
 
-let copy = arr[0];
+let firstElementCopy = arr[0];
 
 for(let i=0 ; i<arr.length - 1; i++){
   arr[i] = arr[i+1];
 }
 
-arr[arr.length - 1] = copy;
+arr[arr.length - 1] = firstElementCopy;
+console.log(arr);
+
+//This code is for right rotation
+
+let arr = [1,2,3,4,5];
+
+let lastElementCopy = arr[arr.length-1];
+
+for(let i=arr.length -1 ; i>0; i--){
+  arr[i] = arr[i-1];
+}
+
+arr[0] = lastElementCopy;
 console.log(arr);
