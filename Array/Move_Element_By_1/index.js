96a46a5b3b1b9d8062c4721593f3595f14acb7af
Ronak1215 <ronakchitalia28@gmail.com>
2025-04-03 10:49:59 +0530

    reverse block swape algorithm code added

--- Array/Move_Element_By_1/index.js
+++ Array/Move_Element_By_1/index.js
@@ -1,4 +1,4 @@
-// this code is for left rotation
+// // this code is for left rotation
 
 let arr = [1,2,3,4,5];
 
@@ -52,5 +52,53 @@ for(let i = 0; i<k ; i++){
   }
   arr[0] = lastElementCopy
 }
+
+console.log(arr)
+
+// this code is for left rotation for k times (efficient method then above but takes more space)
+
+let arr = [1,2,3,4,5]
+let temp = new Array(arr.length)
+let l = 2;
+
+for(i=0; i < arr.length; i++){
+  temp[i] = arr[(i + l) % arr.length]
+}
+
+console.log(temp)
+
+// this code is for left rotation for k times (efficient method then above but takes more space)
+
+
+let arr = [1,2,3,4,5]
+let temp = new Array(arr.length)
+let l = 2;
+
+for(i=0; i < arr.length; i++){
+  temp[(i + l) % arr.length] = arr[i]
+}
+
+console.log(temp)
+
+
+// this code is for left rotation for k times (efficient method then above)
+
+let arr = [1,2,3,4,5]
+
+let k = 2
+
+function reverse(i,j){
+  while(i<j){
+    let temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+    i++;
+    j--;
+  }
+}
+
+reverse(0,k-1)
+reverse(k,arr.length-1)
+reverse(0,arr.length-1)
 
 console.log(arr)
